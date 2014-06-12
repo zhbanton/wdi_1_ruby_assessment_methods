f9bfe8087787482af9f996507216cd996734d55f
zhbanton <zhbanton@gmail.com>
2014-06-12 10:37:24 -0400

    Finished Assessment!

--- methods.rb
+++ methods.rb
@@ -7,6 +7,9 @@ end
 y = square(2)
 # What is the value of 'x' after this line executes?
 
+# x doesnt exist within the scope of y = square(2); if you tried to access it, ruby would
+# throw and error
+
 
 #### Question 2
 # What is the value of `age` after we call `get_older`?
@@ -17,6 +20,8 @@ end
 
 age = 10
 get_older(age)
+
+# 10 - the method returns age + 1 but doesnt actually modify age itself
 
 
 #### Question 3
@@ -34,11 +39,18 @@ end
 energy = 2
 jump
 
+# You would get an error in the how_high method- because energy is not passed in as
+# a parameter to the method, it won't know what you're talking about
+
 
 #### Question 4
 # Write a method that accepts a string and an optional multiplier (defaults to 3)
 # and returns the original string multiplied that many times (using the * operator).
 # Do not use an options hash or keyword arguments.
+
+def string_mul (str, multiplier = 3)
+  str * multiplier
+end
 
 
 #### Question 5
@@ -48,3 +60,5 @@ def greeting(name:, salutation: 'Hi', volume: :low)
   greeting = "#{salutation} there, #{name}"
   volume == :high ? greeting.upcase : greeting
 end
+
+greeting(name: "puny humans", salutation: 'hello', volume: :low)
